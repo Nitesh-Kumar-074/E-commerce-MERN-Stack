edcb6926bcaf67acb0c40db494039e49c83dac3f
Nitesh-Kumar-074 <niteshkumar94720@gmail.com>
2025-06-23 00:11:43 +0530

    Updated carousel images

--- Client/src/components/Carousel/MainCarouselData.js
+++ Client/src/components/Carousel/MainCarouselData.js
@@ -1,16 +1,19 @@
 const MainCarouselData = [
        {
-              image : "https://static.vecteezy.com/system/resources/thumbnails/048/912/287/small/excited-girl-open-up-shopping-bags-and-gasping-amazed-checking-out-gifts-for-her-with-happy-face-standing-against-pink-background-photo.jpg"
+              image : "https://images.pexels.com/photos/1300550/pexels-photo-1300550.jpeg?cs=srgb&dl=pexels-thelazyartist-1300550.jpg&fm=jpg"
        },
        {
               image : "https://www.mohifashion.com/cdn/shop/articles/Mohi_Bridal_Banner_3.jpg?v=1716554794"
        },
        {
-              image : "https://static.vecteezy.com/system/resources/thumbnails/047/965/074/small/happy-man-looking-surprised-at-shopping-bags-buying-souvenirs-on-vacation-standing-over-yellow-background-photo.jpg"
+              
+              image : "https://www.sliderrevolution.com/wp-content/uploads/2023/06/fashion-website-templates.jpg"
        },
        {
-              image : "https://www.shutterstock.com/image-photo/mockup-white-black-heather-tshirt-260nw-2319467405.jpg"
+              image : "https://wallpapers.com/images/hd/mens-fashion-street-photography-0tt2kh6u8d91tr87.jpg"
        }
 ]
 
 export {MainCarouselData}
+
+// "https://i.pinimg.com/1200x/7e/d0/7d/7ed07dd0533c0e6e1ae94b82f9c0e233.jpg"
